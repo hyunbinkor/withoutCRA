a04dbfeea98d01890d67bd3dd2cb9c0e49d270a1
hbjun <gosqlsz@gmail.com>
2024-08-08 18:02:54 +0900

    🚧 [checkpoint]

---
+++ src/components/Hook/useFromDate.tsx
@@ -0,0 +1,8 @@
+function useFromDate(
+  date = new Date(),
+) {
+  // 임시로 string 반환
+  return date.toString();
+};
+
+export default useFromDate;
--- src/pages/Code/FileTreeLine.tsx
+++ src/pages/Code/FileTreeLine.tsx
@@ -1,35 +1,40 @@
-import Svg from '@/components/atomic/Svg';
+import Svg from '@/components/Atomic/Svg';
+import useFromDate from '@components/Hook/useFromDate'
 
 interface Props {
   isFolder?: boolean;
   commitMessage: string;
-  commitNumber?: number;
+  mergeNumber?: number;
   commitDate: Date;
 }
 
 function FileTreeLine({
   isFolder = false,
   commitMessage,
-  commitNumber,
+  mergeNumber,
   commitDate,
 }: Props) {
   // 에러 핸들링을 한 번 넣어주면 좋을듯? -> 데이터 유효성 검사를
+
+  const fromCommitDate = useFromDate(commitDate);
 
   return (
     <tr className="filetree-document-line">
       <td className="filetree-document-line-filename">
         <Svg icon={isFolder ? 'folder' : 'file' } />
-        <a className="h3 filetree-document-line-filename-text-text">.github</a> 
+        <a className="font-14 black">.github</a> 
       </td>
       <td className="filetree-document-line-commitmessage">
-        <div className="filetree-document-line-commitmessage-container">
-          <a className="h3 filetree-document-line-commitmessage-text tarnished">chore(ci): update to node 20 (</a>{/*
-          */}<a className="h3 filetree-document-line-commitmessage-text blue" href="">#6774</a>{/*
-          */}<a className="h3 filetree-document-line-commitmessage-text tarnished" href="">)</a>
-        </div>
+        <a className="font-14 gray-300">{commitMessage}</a>
+        { mergeNumber ?? 
+          <>
+            <a className="font-14 gray-300">(</a>
+            <a className="font-14 blue" href="">#6774</a>
+            <a className="font-14 gray-300" href="">)</a>
+        </>}
       </td>
       <td className="filetree-document-line-modifieddate">
-        <a className="h3 filetree-document-line-modifieddate-text tarnished">4 month ago</a> 
+        <a className="font-14 gray-300">{fromCommitDate}</a> 
       </td>
     </tr>
   );
