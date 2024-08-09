95432e19410b5537f2a870a87049270190a7fe45
hbjun <gosqlsz@gmail.com>
2024-08-09 16:45:58 +0900

    ✨ [button] component prototype

---
+++ src/components/atomic/Button.tsx
@@ -0,0 +1,83 @@
+import { Children } from "react";
+import Svg from "./Svg";
+
+interface Props {
+  btnIcon?: // 이거 리스트 만들어서 대체해야 할듯?
+    'folder' | 
+    'file' |
+    'toggle',
+  btnIconColor: string,
+  btnHasToggle: boolean,
+  btnToggleSeperate: boolean,
+  btnSize: 'small' | 'medium',
+  btnBackgroundColor: string,
+  btnHasBorder: boolean,
+  btnBorderColor: string,
+  btnHandleClick: () => (void),
+  btnHandleToggleClick?: () => (void),
+}
+
+function Button({
+  btnIcon,
+  btnIconColor='gray-400',
+  btnHasToggle=false,
+  btnToggleSeperate=false,
+  btnSize='medium',
+  btnBackgroundColor='gray-100',
+  btnHasBorder=false,
+  btnBorderColor='gray-300',
+  btnHandleClick,
+  btnHandleToggleClick,
+}: Props) {
+  
+  if (btnHasToggle && btnToggleSeperate) {
+    return (
+      <>
+        <button
+          className={[
+            `btn-${btnSize}`,
+            `background-${btnBackgroundColor}`,
+            `border-${btnBorderColor}`,
+            btnHasBorder && 'border-1px',
+          ].join(' ')}
+          onClick={btnHandleClick}
+        >
+          {btnIcon && <Svg color={btnIconColor} icon={btnIcon} />}
+        </button>
+          {Children}
+        <button
+          className={[
+            `btn-${btnSize}`,
+            `background-${btnBackgroundColor}`,
+            `border-${btnBorderColor}`,
+            btnHasBorder && 'border-1px',
+          ].join(' ')}
+          onClick={btnHandleToggleClick}
+        >
+          {btnIcon && <Svg color={btnIconColor} icon={'toggle'} />}
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <button
+        className={[
+          `btn-${btnSize}`,
+          `background-${btnBackgroundColor}`,
+          `border-${btnBorderColor}`,
+          btnHasBorder && 'border-1px',
+        ].join(' ')}
+        onClick={btnHandleClick}
+      >
+        {btnIcon && <Svg color={btnIconColor} icon={btnIcon} />}
+        {/* 왜 태그로 Children을 감싸야 하는가? */}
+        <>
+          {Children}
+        </>
+        {btnHasToggle && <Svg color={btnIconColor} icon={'toggle'} />}
+      </button>
+    );
+  };
+};
+
+export default Button;
