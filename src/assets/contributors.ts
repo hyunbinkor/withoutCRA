17c54df6f959d211faa03a0b0667436f0f7dcde2
hbjun <gosqlsz@gmail.com>
2024-08-14 14:00:26 +0900

    🍱 [assets] jfif to jpg

---
+++ src/assets/contributors.ts
@@ -0,0 +1,33 @@
+import ctrb_1 from '@/assets/aside-contributors-item-1.jpg';
+import ctrb_2 from '@/assets/aside-contributors-item-2.png';
+import ctrb_3 from '@/assets/aside-contributors-item-3.png';
+import ctrb_4 from '@/assets/aside-contributors-item-4.jpg';
+import ctrb_5 from '@/assets/aside-contributors-item-5.jpg';
+import ctrb_6 from '@/assets/aside-contributors-item-6.jpg';
+import ctrb_7 from '@/assets/aside-contributors-item-7.png';
+import ctrb_8 from '@/assets/aside-contributors-item-8.png';
+import ctrb_9 from '@/assets/aside-contributors-item-9.jpg';
+import ctrb_10 from '@/assets/aside-contributors-item-10.png';
+import ctrb_11 from '@/assets/aside-contributors-item-11.png';
+import ctrb_12 from '@/assets/aside-contributors-item-12.jpg';
+import ctrb_13 from '@/assets/aside-contributors-item-13.jpg';
+import ctrb_14 from '@/assets/aside-contributors-item-14.jpg';
+
+const contributors = {
+  ctrb_1: ctrb_1,
+  ctrb_2: ctrb_2,
+  ctrb_3: ctrb_3,
+  ctrb_4: ctrb_4,
+  ctrb_5: ctrb_5,
+  ctrb_6: ctrb_6,
+  ctrb_7: ctrb_7,
+  ctrb_8: ctrb_8,
+  ctrb_9: ctrb_9,
+  ctrb_10: ctrb_10,
+  ctrb_11: ctrb_11,
+  ctrb_12: ctrb_12,
+  ctrb_13: ctrb_13,
+  ctrb_14: ctrb_14,
+};
+
+export default contributors;
